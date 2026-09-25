833117463536a35d5fe285123fb665dd7c88e19f
Satish Kumar M <satish@talentbridge.co.in>
2026-09-25 14:45:19 +0530

    Initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Services } from './pages/services/services';
+import { Contact } from './pages/contact/contact';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'services', component: Services },
+  { path: 'contact', component: Contact },
+  { path: '**', redirectTo: '' }
+];
